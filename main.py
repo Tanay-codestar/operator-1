5f8e01d106a7d300ddff1ae2934dd21f6037cb7c
Tanay-codestar <sowbrmp@gmail.com>
2024-12-06 18:52:47 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#arithmetic operators
+num1=698888888888888888888888
+num2=688885555555555555555555555555555555555555555
+print("addition=",(num1+num2))
+print("subtraction=",(num1-num2))
+print("multiply=",(num1*num2))
+print("division=",(4/2))
+print("modulo=",(4%2))
+print("modulo=",(20/8))
